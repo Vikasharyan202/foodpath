f82941a429a6c5a7f96dda5bcd5bc1ffd0cec6ab
202vikash@gmail.com <vikash_aryan@202vikashgmailcoms-MacBook-Air.local>
2024-02-09 20:15:30 +0530

    using useState hook todynamic change and useEffect hook to fetching live data from api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
 import resList from "../utils/mockData";
 
 
+
 const Body = () => {
+
+  // Local state variable - super powerful variable
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=25.6073388&lng=85.1362679");
+
+    const json = await data.json();
+
+    console.log(json);
+    setListOfRestaurants(json.data.success.cards[4].gridWidget.gridElements.infoWithStyle.restaurants)
+  }
+
   return(
     <div className='body'>
-      <div className='search'>Search</div>
+      <div className='filter'>
+        <button className="filter-btn" onClick={() => {
+          const filteredRestaurants = listOfRestaurants.filter((res) => res.info.avgRating > 4);
+
+          setListOfRestaurants(filteredRestaurants);
+        }}>Top Rated Restaurants</button>
+      </div>
       <div className='res-container'>
         {
-          resList.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
+          listOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
         }
         
       </div>
